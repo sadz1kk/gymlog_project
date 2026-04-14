d94dbc744ace3fc1566128d3fecfc67815026b4d
maciek0404 <237802@student.uek.krakow.pl>
2026-04-14 23:35:40 +0200

    Create MENU

--- gymlogbook/urls.py
+++ gymlogbook/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from training.views import exercise_list
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('training.urls')),
+    path('logut/', auth_views.LogoutView.as_view(next_page="/"), name="logout")
 ]
